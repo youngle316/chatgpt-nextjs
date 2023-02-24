8124dee70735a98e5ac036bdcbb42607ec5d0111
杨乐乐 <youngle316@gmail.com>
2023-02-24 10:31:40 +0800

    feat: Add svgr

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true
   },
-}
 
-module.exports = nextConfig
+  images: {
+    domains: ['lh3.googleusercontent.com']
+  },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack']
+    });
+
+    return config;
+  }
+};
+
+module.exports = nextConfig;
