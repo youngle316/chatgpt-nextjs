a1d77db0d37d7c473e1ffd2c66ef9740a293d429
杨乐乐 <youngle316@gmail.com>
2023-03-11 19:06:36 +0800

    feat: button can scroll to bottom

--- src/components/contentContainer/index.tsx
+++ src/components/contentContainer/index.tsx
@@ -3,8 +3,11 @@
 import { useRef, useEffect } from 'react';
 import ChatInput from '../chatInput/ChatInput';
 import TopBar from '../topBar/TopBar';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { showBottomDivRef } from '@/recoil/atom/AtomRef';
+import { ArrowSmallDownIcon } from '@heroicons/react/24/outline';
+import { useScrollToView } from '@/hook/useScrollToView';
+import { useInView } from 'react-intersection-observer';
 
 type ContentContainerProps = {
   children: React.ReactNode;
@@ -12,13 +15,21 @@ type ContentContainerProps = {
 };
 
 function ContentContainer({ children, pageId }: ContentContainerProps) {
-  const messageEndRef = useRef<null | HTMLDivElement>(null);
+  const messageEndRef = useRef<HTMLDivElement | null>(null);
 
-  const setShowBottomDivRef = useSetRecoilState(showBottomDivRef);
+  const [ShowBottomDiv, setShowBottomDivRef] = useRecoilState(showBottomDivRef);
+
+  const scrollIntoView = useScrollToView(ShowBottomDiv);
+
+  const { ref, inView } = useInView();
 
   useEffect(() => {
     setShowBottomDivRef(messageEndRef);
   }, []);
+
+  const scrollTobBottom = () => {
+    scrollIntoView();
+  };
 
   return (
     <div className="main">
@@ -29,11 +40,19 @@ function ContentContainer({ children, pageId }: ContentContainerProps) {
             <div className="h-full w-full overflow-y-auto">
               <div className="flex flex-col items-center text-sm dark:bg-gray-800">
                 {children}
+                <div
+                  ref={messageEndRef}
+                  className="h-32 w-full flex-shrink-0 md:h-48"
+                >
+                  <div ref={ref} />
+                </div>
               </div>
-              <div
-                ref={messageEndRef}
-                className="h-32 w-full flex-shrink-0 md:h-48"
-              ></div>
+              <button
+                onClick={scrollTobBottom}
+                className={`scrollDown ${inView && 'hidden'}`}
+              >
+                <ArrowSmallDownIcon className="m-1 h-4 w-4" />
+              </button>
             </div>
           </div>
         </div>
