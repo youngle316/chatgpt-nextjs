f73e4fe00132e20209d4a7dcc9c8f2cbcaa6ab9b
杨乐乐 <youngle316@gmail.com>
2023-03-19 17:04:12 +0800

    feat: add GitHub login method

--- src/components/chatInput/ChatInput.tsx
+++ src/components/chatInput/ChatInput.tsx
@@ -10,15 +10,13 @@ import {
 } from 'firebase/firestore';
 import { useSession } from 'next-auth/react';
 import { FormEvent, useState } from 'react';
-import { toast } from 'react-hot-toast';
 import { db } from '../../service/firebase/firebase';
 import { useRecoilState } from 'recoil';
 import { parentMessageIdState } from '@/recoil/atom/AtomChat';
 import { showBottomDivRef } from '@/recoil/atom/AtomRef';
 import { useRouter } from 'next/navigation';
 import { useScrollToView } from '@/hook/useScrollToView';
-import TwitterSvg from 'public/assets/twitter.svg';
-import GitHubSvg from 'public/assets/github.svg';
+import Footer from '../Footer';
 
 type ChatProps = {
   chatId: string;
@@ -172,32 +170,12 @@ function ChatInput({ chatId }: ChatProps) {
           </div>
         </div>
       </form>
-      <div className="flex justify-center gap-3 px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
-        <div>
-          Developed using gpt-3.5-turbo API by &nbsp;
-          <a
-            className="underline"
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/transitive-bullshit/chatgpt-api"
-          >
-            chatgpt-api
-          </a>
-        </div>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://twitter.com/youngle316"
-        >
-          <TwitterSvg className="h-4 w-4" />
-        </a>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/youngle316"
-        >
-          <GitHubSvg className="h-4 w-4" />
-        </a>
+
+      <div
+        className="flex justify-center gap-3 px-3 pt-2 pb-3 text-center text-xs text-black/50 
+      dark:text-white/50 md:px-4 md:pt-3 md:pb-6"
+      >
+        <Footer />
       </div>
     </>
   );
