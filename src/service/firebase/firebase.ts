debae680a440bddc580dfe813688aa1b3070884d
sirily11 <32106111+sirily11@users.noreply.github.com>
2023-04-03 05:29:13 +0000

    fix: remove config

--- src/service/firebase/firebase.ts
+++ src/service/firebase/firebase.ts
@@ -4,18 +4,10 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB8xml0DPDA0dxydvwTonQLja83cEUez6Q",
-  authDomain: "metopia.firebaseapp.com",
-  projectId: "metopia",
-  storageBucket: "metopia.appspot.com",
-  messagingSenderId: "825481221211",
-  appId: "1:825481221211:web:84a24e62525574f8461b7e",
-  measurementId: "G-DX1YG2JT7F"
-};
+const firebaseConfig = process.env.FIREBASE_CONFIG as string;
 
 // Initialize Firebase
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(JSON.parse(firebaseConfig));
 const db = getFirestore(app);
 
 export { db };
